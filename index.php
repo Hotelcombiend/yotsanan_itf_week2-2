65cafdd7d055ec7a36d4a8304753befc6f0280f5
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2020-11-06 19:02:32 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="staff1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <div class="collapse-content">
+                    <h4 class="card-title">Xing Yan</h4>
+                    <p class="card-text"><small class="text-muted">She has a high level of responsibility and is able to do the job with great patience and is also able to do paperwork.</small></p>
+                    <i class="fa fa-heart-o text-muted float-right p-1 my-1 mr-3" data-toggle="tooltip" data-placement="top" title="kuy"></i>
+                    <img src="profile.png" class="card-img-bottom" style="height: 40px; width: 40px;" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="staff4.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <div class="collapse-content">
+                    <h4 class="card-title">Meihua</h4>
+                    <p class="card-text"><small class="text-muted">She is a very talented person, can write very complex programs and she can speak 3 languages.</small></p>
+                    <i class="fa fa-heart-o text-muted float-right p-1 my-1 mr-3" data-toggle="tooltip" data-placement="top" title="kuy"></i>
+                    <img src="profile.png" class="card-img-bottom" style="height: 40px; width: 40px;" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="staff6.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <div class="collapse-content">
+                    <h4 class="card-title">Jimmy</h4>
+                    <p class="card-text"><small class="text-muted">He is a man of all abilities. He can write programs. Manage documents And he can also speak in two languages.</small></p>
+                    <i class="fa fa-heart-o text-muted float-right p-1 my-1 mr-3" data-toggle="tooltip" data-placement="top" title="kuy"></i>
+                    <img src="profile.png" class="card-img-bottom" style="height: 40px; width: 40px;" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
